87196f6ab8b64c0227a9442215a85a751ab559cf
Андрій/DeSkree <DeSkree4@gmail.com>
2023-03-20 10:27:12 +0200

    commit

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 # Константи
-TOKEN = "6001901124:AAHx4pkRSbLjHsZlkF7juG7uuhyxchm8I3Q"
+TOKEN = "Токен бота"
 
 DEBUG = True  # Якщо дебаг ввімкнено, то парсинг відбувається з файлу ./parser/KPVC_site.html
 
-MAIN_ADMIN = 1037693984
-CHAT_ID_REP = -918314434
+MAIN_ADMIN = "id головного адміна"
+CHAT_ID_REP = "id чату з репортами"
